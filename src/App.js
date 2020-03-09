d2845c51e32084e4242d7d38c66d31bb91922481
Thisaru <thisaru@eight25media.com>
2020-03-09 14:57:01 +0530

    add login page to routs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>dsa</h1>
+      <h1>JWT AUTH</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+// Our Components
+import Login from './components/Login';
+
+
+// import 
+
+ReactDOM.render( 
+    <Router>
+        <div>
+            {/* <Route path="/" exact component={App} /> */}
+            <Route path="/" component={Login} />
+        </div>
+    </Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+ 
